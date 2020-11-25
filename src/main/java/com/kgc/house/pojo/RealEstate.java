44ddaeb873b6f1c176d82c6b9556530fc1e68264
liubaohu_glb <liubaohu_glb@kgc.com>
2020-11-25 16:33:32 +0800

    house-update

---
+++ src/main/java/com/kgc/house/pojo/RealEstate.java
@@ -0,0 +1,34 @@
+package com.kgc.house.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RealEstate {
+    private Integer id;
+    private String cardId;
+    private String projectName;
+    private String address;
+    private String houseType;
+    private Integer area;
+    private Date buildTime;
+    private User user;
+
+    @Override
+    public String toString() {
+        return "RealEstate{" +
+                "id=" + id +
+                ", cardId='" + cardId + '\'' +
+                ", projectName='" + projectName + '\'' +
+                ", address='" + address + '\'' +
+                ", houseType='" + houseType + '\'' +
+                ", area=" + area +
+                ", buildTime=" + buildTime +
+                ", user=" + user +
+                '}';
+    }
+}
---
+++ src/main/java/com/kgc/house/pojo/User.java
@@ -0,0 +1,19 @@
+package com.kgc.house.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private String cardId;
+    private String name;
+    private Integer gender;
+    private Date createTime;
+    private String password;
+    private Integer status;
+
+}
